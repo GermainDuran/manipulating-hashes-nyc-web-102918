d0b9496ead04242a6ec199109b73ef340bce7617
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-17 21:36:28 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -10,7 +10,7 @@ def second_challenge
   #code your solution here!
   groceries.each do |key,value|
     value.each do |attribute,data|
-      new_arrayattribute.values <<
+     attribute.values << new_array
      end 
   end 
   new_array
